b751751e3214c04a92eeb3cb5fc44e1020542f19
SUN GE <sun.ge@126.com>
2017-12-12 13:54:42 +0800

    name fix

--- windll-python/testPyDasScan.py
+++ windll-python/testPyDasScan.py
@@ -7,9 +7,13 @@ import time
 
 from ctypes import *
 
+<<<<<<< HEAD
+os.chdir('C:\\Program Files\\test\\')
+=======
 os.chdir('C:\\Insitum.App\\')
 
 #os.chdir('C:\\Program Files\\')
+>>>>>>> 52d12119b8333c5501581f336dcbadf876881fb7
 
 d="\x00"*512
 
@@ -23,11 +27,19 @@ print d.strip('\x00')
 windll.kernel32.GetModuleFileNameA(None, c_char_p(f), c_int(512))
 print f.strip('\x00')
 
+<<<<<<< HEAD
+ds = WinDLL('DasScanSimu.dll')
+
+ds.ScanInit();
+
+ds.LoadProtocol(c_char_p("proto1.xml"))
+=======
 ds = WinDLL('SV.CT.DasScan.dll')
 
 ds.ScanInit();
 
 ds.LoadProtocol(c_char_p("axial.xml"))
+>>>>>>> 52d12119b8333c5501581f336dcbadf876881fb7
 
 ds.ScanGo()
 
