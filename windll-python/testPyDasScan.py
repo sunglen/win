52d12119b8333c5501581f336dcbadf876881fb7
SUN GE <sun.ge@126.com>
2017-12-12 13:30:29 +0800

    add some windll python examples

---
+++ windll-python/testPyDasScan.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+import sys
+import time
+
+from ctypes import *
+
+os.chdir('C:\\Insitum.App\\')
+
+#os.chdir('C:\\Program Files\\')
+
+d="\x00"*512
+
+f="\x00"*512
+
+#Current Directory will be changed if chdir
+windll.kernel32.GetCurrentDirectoryA(c_int(512), c_char_p(d))
+print d.strip('\x00')
+
+#Module File Name will be always C:\Python27\python.exe if we run it by python it.py
+windll.kernel32.GetModuleFileNameA(None, c_char_p(f), c_int(512))
+print f.strip('\x00')
+
+ds = WinDLL('SV.CT.DasScan.dll')
+
+ds.ScanInit();
+
+ds.LoadProtocol(c_char_p("axial.xml"))
+
+ds.ScanGo()
+
+#Should use kernel32.SleepEx other than time.sleep to wake by windows timer
+while not ds.IsScanFinished():
+    windll.kernel32.SleepEx(c_long(1000), True)
